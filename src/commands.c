4666695bade97e50882eb909ff3d0dba89a705d8
Alakh Sharma <aralakhs@gmail.com>
2026-04-10 10:43:14 +0530

    Records edit during insert/delete/backspace/newline

--- src/commands.c
+++ src/commands.c
@@ -10,37 +10,50 @@ static size_t currentVisualX(void) {
     return vx;
 }
 
-void commandsInsertChar(int c) {
-    bufferInsertChar(&E.buf, E.cur.pos, (char)c);
-    E.cur.pos++;
+static void applyInsert(size_t pos, const char *data, size_t len) {
+    cursor before = E.cur;
+
+    bufferInsertBytes(&E.buf, pos, data, len);
+    E.cur.pos = pos + len;
     cursorSync(&E.cur, &E.buf);
     E.cur.want_col = currentVisualX();
+
+    historyRecordInsert(pos, data, len, &before, &E.cur);
     E.dirty = 1;
 }
 
-void commandsInsertNewline(void) {
-    bufferInsertChar(&E.buf, E.cur.pos, '\n');
-    E.cur.pos++;
+static void applyDelete(size_t pos, size_t len, size_t new_pos) {
+    cursor before = E.cur;
+    char *deleted = bufferCopyRange(&E.buf, pos, len);
+
+    bufferDeleteRange(&E.buf, pos, len);
+    E.cur.pos = new_pos;
     cursorSync(&E.cur, &E.buf);
-    E.cur.want_col = 0;
+    E.cur.want_col = currentVisualX();
+
+    historyRecordDelete(pos, deleted, len, &before, &E.cur);
+    free(deleted);
     E.dirty = 1;
+}
+
+void commandsInsertChar(int c) {
+    char ch = (char)c;
+    applyInsert(E.cur.pos, &ch, 1);
+}
+
+void commandsInsertNewline(void) {
+    char nl = '\n';
+    applyInsert(E.cur.pos, &nl, 1);
 }
 
 void commandsBackspace(void) {
     if (E.cur.pos == 0) return;
-    bufferDeleteRange(&E.buf, E.cur.pos - 1, 1);
-    E.cur.pos--;
-    cursorSync(&E.cur, &E.buf);
-    E.cur.want_col = currentVisualX();
-    E.dirty = 1;
+    applyDelete(E.cur.pos - 1, 1, E.cur.pos - 1);
 }
 
 void commandsDelete(void) {
     if (E.cur.pos >= E.buf.length) return;
-    bufferDeleteRange(&E.buf, E.cur.pos, 1);
-    cursorSync(&E.cur, &E.buf);
-    E.cur.want_col = currentVisualX();
-    E.dirty = 1;
+    applyDelete(E.cur.pos, 1, E.cur.pos);
 }
 
 void commandsMoveCursor(int key) {
